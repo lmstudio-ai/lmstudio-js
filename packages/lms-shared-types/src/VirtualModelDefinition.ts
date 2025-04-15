abc68d544503438ce2a6307e09e205506dd66312
Rugved Somwanshi <rugved@lmstudio.ai>
2025-04-15 10:50:30 -0400

    Add compatiblity types and update others in virtual model

--- packages/lms-shared-types/src/VirtualModelDefinition.ts
+++ packages/lms-shared-types/src/VirtualModelDefinition.ts
@@ -1,38 +1,73 @@
 import { z, type ZodSchema } from "zod";
 import { kvConfigSchema, type KVConfig } from "./KVConfig.js";
+import {
+  modelCompatibilityTypeSchema,
+  type ModelCompatibilityType,
+} from "./ModelCompatibilityType.js";
+
+/**
+ * The indicator whether the virtual model is trained for tool use.
+ * There could be cases where not all concrete models are trained for tool use.
+ * In that case, we can use the "mixed" value to indicate that.
+ */
+export type VirtualModelTrainedForToolUse = true | false | "mixed";
+
+const virtualModelTrainedForToolUseSchema = z.union([
+  z.boolean(),
+  z.literal("mixed"),
+]) as ZodSchema<VirtualModelTrainedForToolUse>;
+
+/**
+ * The indicator whether the virtual model supports vision.
+ * There could be cases where not all concrete models support vision.
+ * In that case, we can use the "mixed" value to indicate that.
+ */
+export type VirtualModelVisionSupport = true | false | "mixed";
+
+const virtualModelVisionSupportSchema = z.union([
+  z.boolean(),
+  z.literal("mixed"),
+]) as ZodSchema<VirtualModelVisionSupport>;
 
 /**
  * Allows the creator of a model to override certain metadata fields.
  */
 export interface VirtualModelDefinitionMetadataOverrides {
   /**
-   * Architecture of the model. e.g. llama, qwen2, etc.
+   * Architectures of the model. e.g. llama, qwen2, etc.
    */
-  architecture?: string;
+  architectures?: string[];
+  /**
+   * Model Compatibility types of the concrete models.
+   */
+  compatibilityTypes?: ModelCompatibilityType[];
   /**
    * The number of parameters in a short string format. e.g. 7B, 13B, 70B, etc.
    */
-  paramsString?: string;
+  paramsStrings?: string[];
   /**
    * The minimum required memory to load the model in bytes.
    */
   minMemoryUsageBytes?: number;
   /**
    * (LLM only) Whether the model is trained for tool use. Models that are trained for tool use
-   * generally are more capable of using tools effectively.
+   * generally are more capable of using tools effectively. Could be a mixture of tool use and
+   * non-tool use concrete models.
    */
-  trainedForToolUse?: boolean;
+  trainedForToolUse?: VirtualModelTrainedForToolUse;
   /**
-   * (LLM only) Whether the model can take image inputs.
+   * (LLM only) Whether the model can take image inputs. Could be a mixture of image input and
+   * non-image input concrete models.
    */
-  vision?: boolean;
+  vision?: VirtualModelVisionSupport;
 }
 export const virtualModelDefinitionMetadataOverridesSchema = z.object({
-  architecture: z.string().optional(),
-  paramsString: z.string().optional(),
+  architectures: z.array(z.string()).optional(),
+  compatibilityTypes: z.array(modelCompatibilityTypeSchema).optional(),
+  paramsStrings: z.array(z.string()).optional(),
   minMemoryUsageBytes: z.number().optional(),
-  trainedForToolUse: z.boolean().optional(),
-  vision: z.boolean().optional(),
+  trainedForToolUse: virtualModelTrainedForToolUseSchema.optional(),
+  vision: virtualModelVisionSupportSchema.optional(),
 }) as ZodSchema<VirtualModelDefinitionMetadataOverrides>;
 
 export interface VirtualModelDefinition {
