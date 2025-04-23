97ffe57d8ba188920ff393d0ce697d4bcebc79b7
Ryan Huang <ryan@lmstudio.ai>
2025-04-23 16:18:17 -0400

    Allows sliced signals to be short circuited

--- packages/lms-common/src/SlicedSignal.ts
+++ packages/lms-common/src/SlicedSignal.ts
@@ -26,6 +26,14 @@ type PathSegment =
       type: "mapKeyWithDefault";
       key: any;
       default: any;
+    }
+  | {
+      type: "keyWithShortCircuit";
+      key: string | number;
+    }
+  | {
+      type: "mapKeyWithShortCircuit";
+      key: any;
     };
 
 /**
@@ -37,8 +45,24 @@ export function makeSlicedSignalFrom<TSource>(
   return new SlicedSignalBuilderImpl<
     TSource,
     StripNotAvailable<TSource>,
-    TSource extends NotAvailable ? true : false
+    TSource extends NotAvailable ? true : false,
+    false
   >(writableSignal[0], writableSignal[1]);
+}
+
+export function chainMaybeShortCircuitedSignalFrom<TSource>(
+  writableSignal: readonly [signal: SignalLike<TSource | ShortCircuited>, setter: Setter<TSource>],
+) {
+  return new SlicedSignalBuilderImpl<
+    TSource,
+    StripNotAvailable<TSource>,
+    TSource extends NotAvailable ? true : false,
+    true
+  >(writableSignal[0], writableSignal[1]);
+}
+
+export function isShortCircuited(value: any): value is ShortCircuited {
+  return value === shortCircuited;
 }
 
 /**
@@ -47,7 +71,7 @@ export function makeSlicedSignalFrom<TSource>(
  * At any point, if a default value is used, the defaultUsed callback is called with the index of
  * the path segment where the default value was used.
  */
-function drill(value: any, path: Array<PathSegment>, defaultUsed?: (index: number) => void) {
+function drill(value: any, path: Array<PathSegment>, onDefaultUsed?: (index: number) => void) {
   let current = value;
   let index = 0;
   for (const key of path) {
@@ -59,11 +83,10 @@ function drill(value: any, path: Array<PathSegment>, defaultUsed?: (index: numbe
         current = current[key.key];
         break;
       case "keyWithDefault": {
-        if (current[key.key] === undefined) {
+        current = current[key.key];
+        if (current === undefined) {
           current = key.default;
-          defaultUsed?.(index);
-        } else {
-          current = current[key.key];
+          onDefaultUsed?.(index);
         }
         break;
       }
@@ -73,9 +96,21 @@ function drill(value: any, path: Array<PathSegment>, defaultUsed?: (index: numbe
       case "mapKeyWithDefault":
         if (!current.has(key.key)) {
           current = key.default;
-          defaultUsed?.(index);
+          onDefaultUsed?.(index);
         } else {
           current = current.get(key.key);
+        }
+        break;
+      case "keyWithShortCircuit":
+        current = current[key.key];
+        if (current === undefined) {
+          return shortCircuited;
+        }
+        break;
+      case "mapKeyWithShortCircuit":
+        current = current.get(key.key);
+        if (current === undefined) {
+          return shortCircuited;
         }
         break;
       default: {
@@ -127,17 +162,25 @@ function pathStartsWith(path: Array<string | number>, prefix: Array<string | num
   return true;
 }
 
-class SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable extends boolean> {
+export const shortCircuited = Symbol("shortCircuited");
+export type ShortCircuited = typeof shortCircuited;
+
+class SlicedSignalBuilderImpl<
+  TSource,
+  TCurrent,
+  TCanBeNotAvailable extends boolean,
+  TCanBeShortCircuited extends boolean,
+> {
   private readonly accessPath: Array<PathSegment> = [];
   private readonly path: Array<any> = [];
   private readonly tagKey = String(Math.random());
   public constructor(
-    private readonly sourceSignal: SignalLike<TSource>,
+    private readonly sourceSignal: SignalLike<TSource | ShortCircuited>,
     private readonly sourceSetter: Setter<TSource>,
   ) {}
   public access<TKey extends keyof TCurrent & (string | number)>(
     key: TKey,
-  ): SlicedSignalBuilderImpl<TSource, TCurrent[TKey], TCanBeNotAvailable> {
+  ): SlicedSignalBuilderImpl<TSource, TCurrent[TKey], TCanBeNotAvailable, TCanBeShortCircuited> {
     this.accessPath.push({ type: "key", key });
     this.path.push(key);
     return this as any;
@@ -145,8 +188,46 @@ class SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable extends bool
   public accessWithDefault<TKey extends keyof TCurrent & (string | number)>(
     key: TKey,
     defaultValue: TCurrent[TKey],
-  ): SlicedSignalBuilderImpl<TSource, TCurrent[TKey], TCanBeNotAvailable> {
+  ): SlicedSignalBuilderImpl<
+    TSource,
+    Exclude<TCurrent[TKey], undefined>,
+    TCanBeNotAvailable,
+    TCanBeShortCircuited
+  > {
     this.accessPath.push({ type: "keyWithDefault", key, default: defaultValue });
+    this.path.push(key);
+    return this as any;
+  }
+  /**
+   * Access a key with short circuiting. Short circuiting means that, in the case that the key is
+   * not present, instead of failing, the signal will be set to shortCircuited and remainder of the
+   * slicing path is ignored. In addition, the setter will be disabled in that calling the setter
+   * will not do anything.
+   *
+   * This is useful when dealing with zombie children - For example, there is an array of 10
+   * elements, and a component subscribes to the 10th element. When the 10th element is removed, it
+   * may happen that the child component (that renders the 10th element) is updated before the
+   * parent component (that renders the child components). In which case, the child component still
+   * wants to render the content of the 10th element, but the content no longer exists. By accessing
+   * the 10th element with short circuiting, the child component can bail out if the value it
+   * subscribes to is short circuited.
+   *
+   * Short circuiting can be chained as well, in which case chainMaybeShortCircuitedSignalFrom
+   * should be used - When the source signal is short circuited, the sliced signal will also be
+   * short circuited.
+   */
+  public accessWithShortCircuit<TKey extends keyof TCurrent & (string | number)>(
+    key: TKey,
+  ): SlicedSignalBuilderImpl<
+    TSource,
+    Exclude<TCurrent[TKey], undefined>,
+    TCanBeNotAvailable,
+    true // Can short circuit
+  > {
+    this.accessPath.push({
+      type: "keyWithShortCircuit",
+      key,
+    });
     this.path.push(key);
     return this as any;
   }
@@ -155,7 +236,8 @@ class SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable extends bool
   ): SlicedSignalBuilderImpl<
     TSource,
     TCurrent extends Map<any, infer RValue> ? RValue : never,
-    TCanBeNotAvailable
+    TCanBeNotAvailable,
+    TCanBeShortCircuited
   > {
     this.accessPath.push({ type: "mapKey", key });
     this.path.push(key);
@@ -169,22 +251,48 @@ class SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable extends bool
   ): SlicedSignalBuilderImpl<
     TSource,
     TCurrent extends Map<TKey, infer RValue> ? RValue : never,
-    TCanBeNotAvailable
+    TCanBeNotAvailable,
+    TCanBeShortCircuited
   > {
     this.accessPath.push({ type: "mapKeyWithDefault", key, default: defaultValue });
     this.path.push(key);
     return this as any;
   }
+  public mapAccessWithShortCircuit<
+    TKey extends TCurrent extends Map<infer RKey, any> ? RKey : never,
+  >(
+    key: TKey,
+  ): SlicedSignalBuilderImpl<
+    TSource,
+    TCurrent extends Map<any, infer RValue> ? RValue : never,
+    TCanBeNotAvailable,
+    true // Can short circuit
+  > {
+    this.accessPath.push({
+      type: "mapKeyWithShortCircuit",
+      key,
+    });
+    this.path.push(key);
+    return this as any;
+  }
   public done(): readonly [
-    signal: LazySignal<TCurrent | (TCanBeNotAvailable extends true ? NotAvailable : never)>,
+    signal: LazySignal<
+      | TCurrent
+      | (TCanBeNotAvailable extends true ? NotAvailable : never)
+      | (TCanBeShortCircuited extends true ? ShortCircuited : never)
+    >,
     setter: Setter<TCurrent>,
   ] {
     const sourceInitialValue = this.sourceSignal.get();
     const initialValue = isAvailable(sourceInitialValue)
-      ? drill(sourceInitialValue, this.accessPath)
+      ? isShortCircuited(sourceInitialValue)
+        ? shortCircuited
+        : drill(sourceInitialValue, this.accessPath)
       : LazySignal.NOT_AVAILABLE;
     const signal = LazySignal.create<
-      TCurrent | (TCanBeNotAvailable extends true ? NotAvailable : never)
+      | TCurrent
+      | (TCanBeNotAvailable extends true ? NotAvailable : never)
+      | (TCanBeShortCircuited extends true ? ShortCircuited : never)
     >(initialValue, setDownstream => {
       const unsubscribe = this.sourceSignal.subscribeFull((value, patches, tags) => {
         const newPatches: Array<Patch> = [];
@@ -193,18 +301,17 @@ class SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable extends bool
           const relationship = pathRelationship(patch.path, this.path);
           // If one of the ancestors have been replaced, we need to replace the whole value
           if (relationship === "ancestor") {
-            if (patch.op !== "replace") {
-              throw new Error("Only replace patches are supported for ancestor relationships");
-            }
             newPatches.length = 0;
             newPatches.push({
               op: "replace",
               path: [],
-              value: drill(
-                patch.value!,
-                // Get the part that is relevant to this slice
-                this.accessPath.slice(patch.path.length),
-              ),
+              value: isShortCircuited(value)
+                ? shortCircuited
+                : drill(
+                    value,
+                    // Get the part that is relevant to this slice
+                    this.accessPath,
+                  ),
             });
           } else if (relationship === "children") {
             // If one of the children have been replaced, we need to replace the subset that
@@ -216,7 +323,7 @@ class SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable extends bool
           }
           // Otherwise, we don't need to do anything
         }
-        const newValue = drill(value, this.accessPath);
+        const newValue = isShortCircuited(value) ? value : drill(value, this.accessPath);
         const newTags = tags
           .filter(tag => tag.startsWith(this.tagKey))
           .map(tag => tag.slice(this.tagKey.length));
@@ -239,6 +346,9 @@ class SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable extends bool
       const newTags = tags?.map(tag => this.tagKey + tag);
       this.sourceSetter.withPatchUpdater(oldValue => {
         const newPatches: Array<Patch> = [];
+        if (isShortCircuited(oldValue)) {
+          return [oldValue, []] as const;
+        }
         const slicedOldValue = drill(oldValue, this.accessPath, index => {
           // A default value has been used. We need to inject a patch that adds the default value
           // to the parent object.
@@ -256,6 +366,10 @@ class SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable extends bool
             value: defaultValue,
           });
         });
+        if (isShortCircuited(slicedOldValue)) {
+          // If the value is short circuited, we don't want to apply the updater. Just no-op this.
+          return [oldValue, []] as const;
+        }
         const [_newSlicedValue, patches] = updater(slicedOldValue);
         newPatches.push(
           ...patches.map(patch => ({
@@ -275,4 +389,5 @@ export type SlicedSignalBuilder<
   TSource,
   TCurrent,
   TCanBeNotAvailable extends boolean,
-> = SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable>;
+  TCanBeShortCircuited extends boolean,
+> = SlicedSignalBuilderImpl<TSource, TCurrent, TCanBeNotAvailable, TCanBeShortCircuited>;
