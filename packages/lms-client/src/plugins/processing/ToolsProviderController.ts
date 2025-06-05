95d8b3f5dd7978cd383320bfb9cadf69ae23c770
Ryan Huang <ryan@lmstudio.ai>
2025-06-05 15:03:46 -0400

    Add a method to get working directories

--- packages/lms-client/src/plugins/processing/ToolsProviderController.ts
+++ packages/lms-client/src/plugins/processing/ToolsProviderController.ts
@@ -15,7 +15,15 @@ export class ToolsProviderController {
     public readonly client: LMStudioClient,
     private readonly pluginConfig: KVConfig,
     public readonly signal: AbortSignal,
+    private readonly workingDirectoryPath: string | null,
   ) {}
+
+  public getWorkingDirectory(): string {
+    if (this.workingDirectoryPath === null) {
+      throw new Error("This prediction process is not attached to a working directory.");
+    }
+    return this.workingDirectoryPath;
+  }
 
   public getPluginConfig<TVirtualConfigSchematics extends VirtualConfigSchematics>(
     configSchematics: ConfigSchematics<TVirtualConfigSchematics>,
