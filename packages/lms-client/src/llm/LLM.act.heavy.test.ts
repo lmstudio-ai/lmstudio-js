163eec116ebb533b88b2d5d004a90598316bacc0
ryan-the-crayon <157742211+ryan-the-crayon@users.noreply.github.com>
2025-06-12 16:14:40 -0400

    Sync tool call execution by default (#335)
    * Sync tool call execution by default
    
    * Correctly call the dequeue method
    
    * Don't console.info in test

--- packages/lms-client/src/llm/LLM.act.heavy.test.ts
+++ packages/lms-client/src/llm/LLM.act.heavy.test.ts
@@ -1,3 +1,4 @@
+import { makePromise } from "@lmstudio/lms-common";
 import { z } from "zod";
 import {
   type ChatMessage,
@@ -107,5 +108,143 @@ describe("LLM.act", () => {
 
     expect(onRoundEnd).toHaveBeenCalledTimes(2);
     expect(onRoundEnd.mock.calls).toEqual([[0], [1]]);
+  });
+  it("should queue up tool calls", async () => {
+    // In this test, we will make the model call the tool twice in parallel. We will then make the
+    // first tool call be artificially long by using a promise that is not resolved until the
+    // prediction is completed (but not the round).
+
+    const calls: Array<string> = [];
+
+    // Promises that will only be resolved after the prediction is completed. This is to
+    // artificially lengthen the execution of the first tool call.
+    const { promise, resolve } = makePromise<void>();
+
+    const onPredictionCompleted = jest.fn(() => {
+      // Record predictionCompleted
+      calls.push("predictionCompleted");
+      resolve();
+    });
+    const onToolCallRequestEnd = jest.fn();
+    const onToolCallRequestDequeued = jest.fn();
+
+    const additionToolWithQueueControl = tool({
+      name: "add",
+      description: "Add two numbers",
+      parameters: { a: z.number(), b: z.number() },
+      implementation: async ({ a, b }) => {
+        if (a === 1 && b === 3) {
+          calls.push("firstCall");
+          await promise; // Wait for the promise to be resolved
+          return 4;
+        } else if (a === 2 && b === 4) {
+          calls.push("secondCall");
+          return 6;
+        } else {
+          throw new Error(`Unexpected parameters: a=${a}, b=${b}`);
+        }
+      },
+    });
+
+    await model.act(
+      "Calculate 1 + 3 and 2 + 4 with the tool in parallel.",
+      [additionToolWithQueueControl],
+      {
+        temperature: 0,
+        onPredictionCompleted,
+        onToolCallRequestEnd,
+        onToolCallRequestDequeued,
+      },
+    );
+
+    expect(calls).toEqual([
+      "firstCall",
+      "predictionCompleted",
+      // Due to queueing, the second call will only be executed after prediction is completed
+      "secondCall",
+      "predictionCompleted", // Model will say stuff afterwards
+    ]);
+
+    expect(onToolCallRequestEnd).toHaveBeenCalledTimes(2);
+    expect(onToolCallRequestEnd.mock.calls[0]).toMatchObject([
+      0, // roundIndex
+      expect.any(Number), // callId
+      { isQueued: false }, // First call is not queued
+    ]);
+    expect(onToolCallRequestEnd.mock.calls[1]).toMatchObject([
+      0, // roundIndex
+      expect.any(Number), // callId
+      { isQueued: true }, // Second call is queued
+    ]);
+    expect(onToolCallRequestDequeued).toHaveBeenCalledTimes(1);
+  });
+  it("should not queue up tool calls when allowParallelToolExecution is true", async () => {
+    // In this test, we will make the model call the tool twice in parallel. We will then make the
+    // first tool call be artificially long by using a promise that is not resolved until the
+    // prediction is completed (but not the round). However, since this time
+    // allowParallelToolExecution is true, the second tool call will be executed in parallel with
+    // the first one despite the first one taking longer to complete.
+
+    const calls: Array<string> = [];
+
+    // Promises that will only be resolved after the prediction is completed. This is to
+    // artificially lengthen the execution of the first tool call.
+    const { promise, resolve } = makePromise<void>();
+
+    const onPredictionCompleted = jest.fn(() => {
+      // Record predictionCompleted
+      calls.push("predictionCompleted");
+      resolve();
+    });
+    const onToolCallRequestEnd = jest.fn();
+    const onToolCallRequestDequeued = jest.fn();
+
+    const additionToolWithQueueControl = tool({
+      name: "add",
+      description: "Add two numbers",
+      parameters: { a: z.number(), b: z.number() },
+      implementation: async ({ a, b }) => {
+        if (a === 1 && b === 3) {
+          calls.push("firstCall");
+          await promise; // Wait for the promise to be resolved
+          return 4;
+        } else if (a === 2 && b === 4) {
+          calls.push("secondCall");
+          return 6;
+        } else {
+          throw new Error(`Unexpected parameters: a=${a}, b=${b}`);
+        }
+      },
+    });
+
+    await model.act(
+      "Calculate 1 + 3 and 2 + 4 with the tool in parallel.",
+      [additionToolWithQueueControl],
+      {
+        temperature: 0,
+        onPredictionCompleted,
+        onToolCallRequestEnd,
+        onToolCallRequestDequeued,
+        allowParallelToolExecution: true,
+      },
+    );
+
+    expect(calls).toEqual([
+      "firstCall",
+      "secondCall",
+      "predictionCompleted", // First prediction completed
+      "predictionCompleted", // Model will say stuff afterwards
+    ]);
+    expect(onToolCallRequestEnd.mock.calls[0]).toMatchObject([
+      0, // roundIndex
+      expect.any(Number), // callId
+      { isQueued: false }, // First call is not queued
+    ]);
+    expect(onToolCallRequestEnd.mock.calls[1]).toMatchObject([
+      0, // roundIndex
+      expect.any(Number), // callId
+      { isQueued: false }, // Second call is also not queued
+    ]);
+    expect(onToolCallRequestDequeued).toHaveBeenCalledTimes(0);
   });
 });
