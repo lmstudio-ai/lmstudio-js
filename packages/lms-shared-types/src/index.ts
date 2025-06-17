014944110f4e3f505b551008f0e41471c001935a
Matt Clayton <matt@lmstudio.ai>
2025-06-17 11:34:16 -0400

    Format

--- packages/lms-shared-types/src/index.ts
+++ packages/lms-shared-types/src/index.ts
@@ -8,13 +8,13 @@ export {
   allowableEnvVarKeys,
   allowableEnvVarKeysSchema,
   AllowableEnvVars,
-  allowableEnvVarsSchema
+  allowableEnvVarsSchema,
 } from "./AllowableEnvVars.js";
 export {
   ArtifactManifest,
   artifactManifestSchema,
   ArtifactType,
-  artifactTypeSchema
+  artifactTypeSchema,
 } from "./ArtifactManifest.js";
 export {
   ArtifactArtifactDependency,
@@ -28,7 +28,7 @@ export {
   ArtifactManifestBase,
   artifactManifestBaseSchema,
   ArtifactModelDependency,
-  artifactModelDependencySchema
+  artifactModelDependencySchema,
 } from "./ArtifactManifestBase.js";
 export { BackendNotification, backendNotificationSchema } from "./BackendNotification.js";
 export {
@@ -51,7 +51,7 @@ export {
   FunctionToolCallRequest,
   functionToolCallRequestSchema,
   ToolCallRequest,
-  toolCallRequestSchema
+  toolCallRequestSchema,
 } from "./ChatHistoryData.js";
 export { CitationSource, citationSourceSchema } from "./CitationSource.js";
 export { ColorPalette, colorPalette, colorPaletteSchema } from "./ColorPalette.js";
@@ -59,11 +59,11 @@ export {
   DiagnosticsLogEvent,
   DiagnosticsLogEventData,
   diagnosticsLogEventDataSchema,
-  diagnosticsLogEventSchema
+  diagnosticsLogEventSchema,
 } from "./diagnostics/DiagnosticsLogEvent.js";
 export {
   EmbeddingLoadModelConfig,
-  embeddingLoadModelConfigSchema
+  embeddingLoadModelConfigSchema,
 } from "./embedding/EmbeddingLoadModelConfig.js";
 export {
   EmbeddingModelAdditionalInfo,
@@ -73,7 +73,7 @@ export {
   EmbeddingModelInstanceAdditionalInfo,
   embeddingModelInstanceAdditionalInfoSchema,
   EmbeddingModelInstanceInfo,
-  embeddingModelInstanceInfoSchema
+  embeddingModelInstanceInfoSchema,
 } from "./embedding/EmbeddingModelInfo.js";
 export {
   attachSerializedErrorData,
@@ -84,19 +84,19 @@ export {
   recreateSerializedError,
   SerializedLMSExtendedError,
   serializedLMSExtendedErrorSchema,
-  serializeError
+  serializeError,
 } from "./Error.js";
 export {
   DocumentParsingLibraryIdentifier,
   documentParsingLibraryIdentifierSchema,
   DocumentParsingOpts,
-  documentParsingOptsSchema
+  documentParsingOptsSchema,
 } from "./files/DocumentParsingOpts.js";
 export {
   FileNamespace,
   fileNamespaceSchema,
   ParsedFileIdentifier,
-  parsedFileIdentifierSchema
+  parsedFileIdentifierSchema,
 } from "./files/FileIdentifier.js";
 export { FileType, fileTypeSchema } from "./files/FileType.js";
 export {
@@ -106,14 +106,14 @@ export {
   gpuSplitConfigSchema,
   gpuSplitStrategies,
   GPUSplitStrategy,
-  gpuSplitStrategySchema
+  gpuSplitStrategySchema,
 } from "./GPUSplitStrategy.js";
 export { jsonSerializableSchema } from "./JSONSerializable.js";
 export {
   kebabCaseRegex,
   kebabCaseSchema,
   kebabCaseWithDotsRegex,
-  kebabCaseWithDotsSchema
+  kebabCaseWithDotsSchema,
 } from "./kebab.js";
 export {
   KVConfig,
@@ -127,12 +127,12 @@ export {
   KVConfigStack,
   KVConfigStackLayer,
   kvConfigStackLayerSchema,
-  kvConfigStackSchema
+  kvConfigStackSchema,
 } from "./KVConfig.js";
 export { ContentBlockStyle, contentBlockStyleSchema } from "./llm/ContentBlockStyle.js";
 export {
   LLMApplyPromptTemplateOpts,
-  llmApplyPromptTemplateOptsSchema
+  llmApplyPromptTemplateOptsSchema,
 } from "./llm/LLMApplyPromptTemplateOpts.js";
 export {
   LLMContextReference,
@@ -140,7 +140,7 @@ export {
   llmContextReferenceJsonFileSchema,
   llmContextReferenceSchema,
   LLMContextReferenceYamlFile,
-  llmContextReferenceYamlFileSchema
+  llmContextReferenceYamlFileSchema,
 } from "./llm/LLMContextReference.js";
 export {
   GPUSetting,
@@ -160,7 +160,7 @@ export {
   llmMlxKvCacheGroupSizeTypesSchema,
   llmMlxKvCacheQuantizationSchema,
   LLMSplitStrategy,
-  llmSplitStrategySchema
+  llmSplitStrategySchema,
 } from "./llm/LLMLoadModelConfig.js";
 export {
   LLMAdditionalInfo,
@@ -170,7 +170,7 @@ export {
   LLMInstanceAdditionalInfo,
   llmInstanceAdditionalInfoSchema,
   LLMInstanceInfo,
-  llmInstanceInfoSchema
+  llmInstanceInfoSchema,
 } from "./llm/LLMModelInfo.js";
 export {
   LLMContextOverflowPolicy,
@@ -186,7 +186,7 @@ export {
   llmPredictionConfigInputSchema,
   llmPredictionConfigSchema,
   LLMReasoningParsing,
-  llmReasoningParsingSchema
+  llmReasoningParsingSchema,
 } from "./llm/LLMPredictionConfig.js";
 export {
   LLMPredictionFragment,
@@ -194,7 +194,7 @@ export {
   llmPredictionFragmentInputOptsSchema,
   LLMPredictionFragmentReasoningType,
   llmPredictionFragmentReasoningTypeSchema,
-  llmPredictionFragmentSchema
+  llmPredictionFragmentSchema,
 } from "./llm/LLMPredictionFragment.js";
 export {
   LLMGenInfo,
@@ -202,7 +202,7 @@ export {
   LLMPredictionStats,
   llmPredictionStatsSchema,
   LLMPredictionStopReason,
-  llmPredictionStopReasonSchema
+  llmPredictionStopReasonSchema,
 } from "./llm/LLMPredictionStats.js";
 export {
   LLMJinjaPromptTemplate,
@@ -212,13 +212,13 @@ export {
   LLMPromptTemplate,
   llmPromptTemplateSchema,
   LLMPromptTemplateType,
-  llmPromptTemplateTypeSchema
+  llmPromptTemplateTypeSchema,
 } from "./llm/LLMPromptTemplate.js";
 export {
   LLMStructuredPredictionSetting,
   llmStructuredPredictionSettingSchema,
   LLMStructuredPredictionType,
-  llmStructuredPredictionTypeSchema
+  llmStructuredPredictionTypeSchema,
 } from "./llm/LLMStructuredPredictionSetting.js";
 export {
   LLMTool,
@@ -227,15 +227,15 @@ export {
   llmToolParametersSchema,
   llmToolSchema,
   LLMToolUseSetting,
-  llmToolUseSettingSchema
+  llmToolUseSettingSchema,
 } from "./llm/LLMToolUseSetting.js";
 export {
   PredictionLoopHandlerUpdate,
-  predictionLoopHandlerUpdateSchema
+  predictionLoopHandlerUpdateSchema,
 } from "./llm/processing/PredictionLoopHandlerUpdate.js";
 export {
   PreprocessorUpdate,
-  preprocessorUpdateSchema
+  preprocessorUpdateSchema,
 } from "./llm/processing/PreprocessorUpdate.js";
 export {
   ProcessingRequest,
@@ -252,7 +252,7 @@ export {
   processingRequestSchema,
   ProcessingRequestTextInput,
   processingRequestTextInputSchema,
-  ProcessingRequestType
+  ProcessingRequestType,
 } from "./llm/processing/ProcessingRequest.js";
 export {
   BlockLocation,
@@ -304,7 +304,7 @@ export {
   ToolStatusStepState,
   toolStatusStepStateSchema,
   ToolStatusStepStateStatus,
-  toolStatusStepStateStatusSchema
+  toolStatusStepStateStatusSchema,
 } from "./llm/processing/ProcessingUpdate.js";
 export { GetModelOpts, getModelOptsSchema } from "./llm/processing/Processor.js";
 export { ModelCompatibilityType, modelCompatibilityTypeSchema } from "./ModelCompatibilityType.js";
@@ -313,38 +313,38 @@ export {
   HuggingFaceModelDownloadSource,
   huggingFaceModelDownloadSourceSchema,
   ModelDownloadSource,
-  modelDownloadSourceSchema
+  modelDownloadSourceSchema,
 } from "./ModelDownloadSource.js";
 export {
   ModelInfo,
   modelInfoSchema,
   ModelInstanceInfo,
-  modelInstanceInfoSchema
+  modelInstanceInfoSchema,
 } from "./ModelInfo.js";
 export {
   ModelInfoBase,
   modelInfoBaseSchema,
   ModelInstanceInfoBase,
-  modelInstanceInfoBaseSchema
+  modelInstanceInfoBaseSchema,
 } from "./ModelInfoBase.js";
 export { ModelManifest, modelManifestSchema } from "./ModelManifest.js";
 export {
   ModelQuery,
   modelQuerySchema,
   ModelSpecifier,
-  modelSpecifierSchema
+  modelSpecifierSchema,
 } from "./ModelSpecifier.js";
 export {
   fileNameRegex,
   fileNameSchema,
   relativePathNoLeadingDotSlashRegex,
-  relativePathNoLeadingDotSlashSchema
+  relativePathNoLeadingDotSlashSchema,
 } from "./path.js";
 export {
   PluginManifest,
   pluginManifestSchema,
   PluginRunnerType,
-  pluginRunnerTypeSchema
+  pluginRunnerTypeSchema,
 } from "./PluginManifest.js";
 export { PresetManifest, presetManifestSchema } from "./PresetManifest.js";
 export { reasonableKeyStringSchema } from "./reasonable.js";
@@ -356,17 +356,17 @@ export {
   artifactDownloadPlanNodeSchema,
   ArtifactDownloadPlanNodeState,
   artifactDownloadPlanNodeStateSchema,
-  artifactDownloadPlanSchema
+  artifactDownloadPlanSchema,
 } from "./repository/ArtifactDownloadPlan.js";
 export {
   LocalArtifactFileEntry,
   localArtifactFileEntrySchema,
   LocalArtifactFileList,
-  localArtifactFileListSchema
+  localArtifactFileListSchema,
 } from "./repository/ArtifactUpload.js";
 export {
   DownloadProgressUpdate,
-  downloadProgressUpdateSchema
+  downloadProgressUpdateSchema,
 } from "./repository/DownloadProgressUpdate.js";
 export {
   ModelSearchOpts,
@@ -377,24 +377,24 @@ export {
   ModelSearchResultEntryData,
   modelSearchResultEntryDataSchema,
   ModelSearchResultIdentifier,
-  modelSearchResultIdentifierSchema
+  modelSearchResultIdentifierSchema,
 } from "./repository/ModelSearch.js";
 export {
   InternalRetrievalResult,
   InternalRetrievalResultEntry,
   internalRetrievalResultEntrySchema,
-  internalRetrievalResultSchema
+  internalRetrievalResultSchema,
 } from "./retrieval/InternalRetrievalResult.js";
 export { RetrievalChunk, retrievalChunkSchema } from "./retrieval/RetrievalChunk.js";
 export {
   RetrievalChunkingMethod,
   RetrievalChunkingMethodIdentifier,
   retrievalChunkingMethodSchema,
-  serializeRetrievalChunkingMethod
+  serializeRetrievalChunkingMethod,
 } from "./retrieval/RetrievalChunkingMethod.js";
 export {
   RetrievalFileProcessingStep,
-  retrievalFileProcessingStepSchema
+  retrievalFileProcessingStepSchema,
 } from "./retrieval/RetrievalFileProcessingStep.js";
 export {
   Accelerator,
@@ -402,7 +402,7 @@ export {
   AcceleratorType,
   acceleratorTypeSchema,
   Runtime,
-  runtimeSchema
+  runtimeSchema,
 } from "./Runtime.js";
 export {
   KVConfigSchematicsDeserializationError,
@@ -410,7 +410,7 @@ export {
   SerializedKVConfigSchematics,
   SerializedKVConfigSchematicsField,
   serializedKVConfigSchematicsFieldSchema,
-  serializedKVConfigSchematicsSchema
+  serializedKVConfigSchematicsSchema,
 } from "./SerializedKVConfigSchematics.js";
 export {
   BooleanOrMixed,
@@ -440,7 +440,6 @@ export {
   VirtualModelStringCustomFieldDefinition,
   virtualModelStringCustomFieldDefinitionSchema,
   VirtualModelSuggestion,
-  virtualModelSuggestionSchema
+  virtualModelSuggestionSchema,
 } from "./VirtualModelDefinition.js";
 export { zodSchemaSchema } from "./Zod.js";
-
