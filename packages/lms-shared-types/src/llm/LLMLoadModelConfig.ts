5037e5d9cd5c811d0dcf33079ebfa7ce1345f38f
Christian Zhou-Zheng <christian@lmstudio.ai>
2025-08-06 16:50:28 -0400

    oops: type

--- packages/lms-shared-types/src/llm/LLMLoadModelConfig.ts
+++ packages/lms-shared-types/src/llm/LLMLoadModelConfig.ts
@@ -44,7 +44,7 @@ export type GPUSetting = {
    * Maximum number of expert layers to offload to the GPU.
    * Any expert layers above this number will be kept in CPU RAM.
    */
-  maxExpertLayersOffload?: number;
+  maxExpertLayersOffload?: LLMLlamaAccelerationOffloadRatio;
   /**
    * The index of the GPU to use as the main GPU.
    */
