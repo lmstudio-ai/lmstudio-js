8692d7143d3b856437e8cfba988dd6a0800ca3c1
Rugved Somwanshi <rugved@lmstudio.ai>
2025-09-12 17:22:44 -0400

    Comments addressed

--- packages/lms-shared-types/src/diagnostics/DiagnosticsLogEvent.ts
+++ packages/lms-shared-types/src/diagnostics/DiagnosticsLogEvent.ts
@@ -1,43 +1,57 @@
 import { z } from "zod";
-import { type LogLevel } from "../index.js";
+import { type LogLevel, logLevelSchema } from "../LogLevel.js";
 import { llmPredictionStatsSchema, type LLMPredictionStats } from "../llm/LLMPredictionStats.js";
 
+export const diagnosticsLogInputEventDataSchema = z.object({
+  type: z.literal("llm.prediction.input"),
+  input: z.string(),
+  modelPath: z.string(),
+  modelIdentifier: z.string(),
+});
+
+export const diagnosticsLogOutputEventDataSchema = z.object({
+  type: z.literal("llm.prediction.output"),
+  output: z.string(),
+  stats: llmPredictionStatsSchema.optional(),
+  modelIdentifier: z.string(),
+});
+
+export const diagnosticsLogServerEventDataSchema = z.object({
+  type: z.literal("server.log"),
+  content: z.string(),
+  level: logLevelSchema,
+});
+
 export const diagnosticsLogEventDataSchema = z.discriminatedUnion("type", [
-  z.object({
-    type: z.literal("llm.prediction.input"),
-    input: z.string(),
-    modelPath: z.string(),
-    modelIdentifier: z.string(),
-  }),
-  z.object({
-    type: z.literal("llm.prediction.output"),
-    output: z.string(),
-    stats: llmPredictionStatsSchema.optional(),
-    modelPath: z.string(),
-    modelIdentifier: z.string(),
-  }),
-  z.object({
-    type: z.literal("server.log"),
-    content: z.string(),
-    level: z.enum(["debug", "info", "warn", "error"]),
-  }),
+  diagnosticsLogInputEventDataSchema,
+  diagnosticsLogOutputEventDataSchema,
+  diagnosticsLogServerEventDataSchema,
 ]);
 
-type DiagnosticsLogInputEventData = {
+/**
+ * @public
+ */
+export type DiagnosticsLogInputEventData = {
   type: "llm.prediction.input";
   input: string;
   modelPath: string;
   modelIdentifier: string;
 };
-type DiagnosticsLogOutputEventData = {
+
+/**
+ * @public
+ */
+export type DiagnosticsLogOutputEventData = {
   type: "llm.prediction.output";
   output: string;
   stats?: LLMPredictionStats;
-  modelPath: string;
   modelIdentifier: string;
 };
 
-type DiagnosticsLogServerEventData = {
+/**
+ * @public
+ */
+export type DiagnosticsLogServerEventData = {
   type: "server.log";
   content: string;
   level: LogLevel;
@@ -55,6 +69,7 @@ export const diagnosticsLogEventSchema = z.object({
   timestamp: z.number(),
   data: diagnosticsLogEventDataSchema,
 });
+
 /**
  * @public
  */
