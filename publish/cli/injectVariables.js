fbcb26504b2fbf7729985156da02c5d8a8e12e28
rugvedS07 <rugved@lmstudio.ai>
2026-01-02 14:01:38 -0500

    Commit hash `lms` versions (#504)

--- publish/cli/injectVariables.js
+++ publish/cli/injectVariables.js
@@ -1,15 +1,15 @@
-// Inject the current version by replacing the magic string <LMS-CLI-CURRENT-VERSION>
+// Inject commit hash by replacing the magic string <LMS-CLI-COMMIT-HASH>
 // This is much faster than rollup-plugin-replace
 
 import { readFileSync, writeFileSync } from "fs";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import { execSync } from "child_process";
 
 const currentFilePath = fileURLToPath(import.meta.url);
 const currentDirectoryPath = dirname(currentFilePath);
 
 const content = readFileSync(join(currentDirectoryPath, "dist", "index.js"), "utf-8");
-const packageJson = readFileSync(join(currentDirectoryPath, "package.json"), "utf-8");
 let lmsKey = null;
 try {
   lmsKey = readFileSync(join(currentDirectoryPath, "lms-key"), "utf-8").trim();
@@ -17,7 +17,24 @@ try {
   console.error("Failed to read lms-key. Build in development mode.");
 }
 
-let replaced = content.replaceAll("<LMS-CLI-CURRENT-VERSION>", JSON.parse(packageJson).version);
+const lmsCliPath = join(currentDirectoryPath, "..", "..", "packages", "lms-cli");
+let commitHash = "";
+try {
+  commitHash = execSync("git rev-parse --short HEAD", {
+    cwd: lmsCliPath,
+    encoding: "utf-8",
+  }).trim();
+} catch (error) {
+  console.error("Failed to get commit hash from ../../packages/lms-cli submodule");
+  throw error;
+}
+
+// Just a sanity check
+if (commitHash.length === 0) {
+  throw new Error("Commit hash is empty");
+}
+
+let replaced = content.replaceAll("<LMS-CLI-COMMIT-HASH>", commitHash);
 if (lmsKey !== null) {
   replaced = replaced.replaceAll("<LMS-CLI-LMS-KEY>", lmsKey);
 }
