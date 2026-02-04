f14a26603a3f22b8b2140b6a5d6762040c6684c1
Ryan Huang <ryan@lmstudio.ai>
2026-02-04 16:01:08 -0500

    Add instance reference

--- packages/lms-client/src/embedding/EmbeddingModel.ts
+++ packages/lms-client/src/embedding/EmbeddingModel.ts
@@ -32,6 +32,13 @@ export class EmbeddingModel
   public readonly displayName: string;
   public readonly sizeBytes: number;
   /**
+   * Unique stable identifier for the model instance.
+   *
+   * @deprecated [DEP-LOW-LEVEL] This API is not guaranteed to not change nor to continue to exist
+   * in the future.
+   */
+  public readonly instanceReference: string;
+  /**
    * A more stable identifier for the model that mostly consists of path to the model. Can be very
    * long. Not suitable for displaying. Use `modelKey` for most purposes.
    *
@@ -58,6 +65,7 @@ export class EmbeddingModel
     this.format = info.format;
     this.displayName = info.displayName;
     this.sizeBytes = info.sizeBytes;
+    this.instanceReference = info.instanceReference;
     this.indexedModelIdentifier = info.indexedModelIdentifier;
   }
   public async unload() {
--- packages/lms-client/src/llm/LLM.ts
+++ packages/lms-client/src/llm/LLM.ts
@@ -34,6 +34,13 @@ export class LLM
   public readonly vision: boolean;
   public readonly trainedForToolUse: boolean;
   /**
+   * Unique stable identifier for the model instance.
+   *
+   * @deprecated [DEP-LOW-LEVEL] This API is not guaranteed to not change nor to continue to exist
+   * in the future.
+   */
+  public readonly instanceReference: string;
+  /**
    * A more stable identifier for the model that mostly consists of path to the model. Can be very
    * long. Not suitable for displaying. Use `modelKey` for most purposes.
    *
@@ -62,6 +69,7 @@ export class LLM
     this.sizeBytes = info.sizeBytes;
     this.vision = info.vision;
     this.trainedForToolUse = info.trainedForToolUse;
+    this.instanceReference = info.instanceReference;
     this.indexedModelIdentifier = info.indexedModelIdentifier;
   }
   public async unload() {
