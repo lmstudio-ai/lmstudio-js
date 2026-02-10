640b410247714f873b42a6579b879a482b445061
azisislm <alex.zisis@lmstudio.ai>
2026-02-10 11:41:31 -0500

    add error display data code

--- packages/lms-shared-types/src/GenericErrorDisplayData.ts
+++ packages/lms-shared-types/src/GenericErrorDisplayData.ts
@@ -39,6 +39,9 @@ export type GenericErrorDisplayData =
       supportedVersion: string | null;
     }
   | {
+      code: "generic.manageRuntimes";
+    }
+  | {
       code: "generic.presetNotFound";
       specifiedFuzzyPresetIdentifier: string;
       availablePresetsSample: Array<{
@@ -82,6 +85,9 @@ export const genericErrorDisplayDataSchema = [
     engineType: z.string(),
     installedVersion: z.string(),
     supportedVersion: z.string().nullable(),
+  }),
+  z.object({
+    code: z.literal("generic.manageRuntimes"),
   }),
   z.object({
     code: z.literal("generic.presetNotFound"),
