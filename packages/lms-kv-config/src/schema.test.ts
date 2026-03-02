d11401327aa821421855aa6379e7814ef2a80ba6
will-lms <will@lmstudio.ai>
2026-03-02 16:38:30 -0500

    Add Presence Penalty (#552)

--- packages/lms-kv-config/src/schema.test.ts
+++ packages/lms-kv-config/src/schema.test.ts
@@ -92,6 +92,36 @@ describe("globalConfigSchematics", () => {
         ),
       ).toBe(true);
     });
+    it("should work with presence penalty", () => {
+      expect(
+        globalConfigSchematics.fieldEffectiveEquals(
+          "llm.prediction.llama.presencePenalty",
+          { checked: true, value: 0 },
+          { checked: true, value: 0 },
+        ),
+      ).toBe(true);
+      expect(
+        globalConfigSchematics.fieldEffectiveEquals(
+          "llm.prediction.llama.presencePenalty",
+          { checked: true, value: 0 },
+          { checked: true, value: 1 },
+        ),
+      ).toBe(false);
+      expect(
+        globalConfigSchematics.fieldEffectiveEquals(
+          "llm.prediction.llama.presencePenalty",
+          { checked: true, value: 0 },
+          { checked: false, value: 0 },
+        ),
+      ).toBe(false);
+      expect(
+        globalConfigSchematics.fieldEffectiveEquals(
+          "llm.prediction.llama.presencePenalty",
+          { checked: false, value: 0 },
+          { checked: false, value: 1 },
+        ),
+      ).toBe(true);
+    });
   });
   describe("stringify", () => {
     it("should work with temperature", () => {
@@ -122,6 +152,42 @@ describe("globalConfigSchematics", () => {
       expect(
         globalConfigSchematics.stringifyField(
           "llm.prediction.repeatPenalty",
+          {
+            checked: false,
+            value: 0.5,
+          },
+          {
+            t: translateFn,
+          },
+        ),
+      ).toBe("TEST");
+      expect(translateFn).toHaveBeenCalledTimes(1);
+      expect(translateFn).toHaveBeenCalledWith("config:customInputs.checkboxNumeric.off", "OFF");
+    });
+    it("should work with presence penalty", () => {
+      expect(
+        globalConfigSchematics.stringifyField("llm.prediction.llama.presencePenalty", {
+          checked: true,
+          value: 0,
+        }),
+      ).toBe("0.00");
+      expect(
+        globalConfigSchematics.stringifyField("llm.prediction.llama.presencePenalty", {
+          checked: true,
+          value: 0.5,
+        }),
+      ).toBe("0.50");
+      expect(
+        globalConfigSchematics.stringifyField("llm.prediction.llama.presencePenalty", {
+          checked: false,
+          value: 0.5,
+        }),
+      ).toBe("OFF");
+
+      const translateFn = jest.fn(() => "TEST");
+      expect(
+        globalConfigSchematics.stringifyField(
+          "llm.prediction.llama.presencePenalty",
           {
             checked: false,
             value: 0.5,
