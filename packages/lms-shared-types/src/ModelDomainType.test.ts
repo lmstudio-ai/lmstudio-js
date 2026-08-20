d3d3b27891c1ed8ac432db6c88aef5350ff1f4f1
Will Burford <will@lmstudio.ai>
2026-08-20 16:20:30 -0400

    Remove trivial model domain test

--- packages/lms-shared-types/src/ModelDomainType.test.ts
+++
@@ -1,7 +0,0 @@
-import { modelDomainTypeSchema } from "./ModelDomainType.js";
-
-describe("ModelDomainType", () => {
-  it("accepts drafter model domains", () => {
-    expect(modelDomainTypeSchema.parse("drafter")).toBe("drafter");
-  });
-});
