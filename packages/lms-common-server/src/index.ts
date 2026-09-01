d1cc962aaef3238907db9fb36f3897550b54e382
Rugved Somwanshi <rugved@lmstudio.ai>
2026-09-01 12:42:11 -0400

    Expose API server status lookup

--- packages/lms-common-server/src/index.ts
+++ packages/lms-common-server/src/index.ts
@@ -3,9 +3,11 @@ export { findLMStudioHome } from "./findLMStudioHome.js";
 export {
   APIServerStatus,
   findOrStartLlmster,
+  getAPIServerStatusOrThrow,
   getLocalAPIServerStatusAtPortOrThrow,
   tryFindLocalAPIServer,
   type FindOrStartLlmsterOptions,
+  type GetAPIServerStatusOrThrowOpts,
 } from "./findOrStartLlmster.js";
 export { PlainTextFileData } from "./PlainTextFileData.js";
 export { SimpleFileData } from "./SimpleFileData.js";
